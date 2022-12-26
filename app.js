d550ca30341a3ebd23dc52d785844a26badbdf32
mauricevalerio <75749759+mauricevalerio@users.noreply.github.com>
2022-12-26 00:16:37 -0800

    Fixed bugs
    Prepend latest saved content to display it on top.
    Kept the page focused on the main content if list gets longer by adding min-height to body

--- app.js
+++ app.js
@@ -16,69 +16,65 @@ function getDateTime() {
 	return `${date} ${time}`;
 };
 
-function getMonth() {
+function getMonth() { //increments month number to 1
 	let month = new Date();
 	return month.getMonth() + 1;
 };
 
-function padZero (time) {
+function padZero(time) { //prepends/pads 0 to hours, minutes, seconds if it is a single digit
 	return time.toString().length === 1 ? time.toString().padStart(2, '0') : time;
 };
 
-function removeReminderMsg() {
-	if (document.getElementById("small-message")) {
-		document.getElementById("small-message").remove();
+function removeErrorMsg() { //removes error msg from DOM
+	if (document.getElementById("error-message")) {
+		document.getElementById("error-message").remove();
 	} else {
 		return;
 	}
 };
 
-function addReminderMsg() {
+function errorMsg() { //adds message when attempting to decrement counter at 0.
 	let message = document.createElement("small");
 	message.textContent = "Count must be greater than or equal to 0!";
 	countObj.numOutput.insertAdjacentElement("afterend", message);
-	message.classList.add("small-message", "spacing");
-	message.setAttribute("id", "small-message");
+	message.classList.add("error-message", "spacing");
+	message.setAttribute("id", "error-message");
 }
 
 countObj.decrementBtn.addEventListener("click", (e) => {
-	e.preventDefault();
 	if (countObj.count) {
 		countObj.count -= 1;
 		countObj.numOutput.textContent = countObj.count;
 	} else {
-		if (!document.getElementById("small-message")) {
-			addReminderMsg();
+		if (!document.getElementById("error-message")) {
+			errorMsg();
 		} else {
 			return;
 		}
 	}
-	
+
 });
 
 countObj.incrementBtn.addEventListener("click", (e) => {
-	e.preventDefault();
 	countObj.count += 1;
 	countObj.numOutput.textContent = countObj.count;
-	removeReminderMsg();
+	removeErrorMsg();
 
 });
 
 countObj.saveBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	
 	let dateList = document.createElement("p");
 	let countList = document.createElement("p");
-	
+
 	dateList.textContent = getDateTime();
 	countList.textContent = countObj.numOutput.textContent;
-	
-	countObj.divDateList.append(dateList);
-	countObj.divCountList.append(countList);
-	
+
+	countObj.divDateList.prepend(dateList);
+	countObj.divCountList.prepend(countList);
+
 	countObj.count = 0;
 	countObj.numOutput.textContent = countObj.count;
-	removeReminderMsg();
+	removeErrorMsg();
 });
 
 
